8ef8b55283e39cad2bb5dac8321bb9fc9fac7b18
David A. Turner <dturn73@gmail.com>
2016-05-22 14:42:48 -0700

    inital commit after messing up previous repo

---
+++ js/custom.js
@@ -0,0 +1,31 @@
+var currentEarthQuake, time;
+
+var getData = function(){
+	var time = new Date($.now()).toLocaleString();
+	$.get('http://earthquake.usgs.gov/fdsnws/event/1/query?',
+		{//build a query string based on USGS's API
+			format: 'geojson',
+			starttime: time,
+			catalog: 'ci',
+			contributor: 'ci',
+			minmagnitude: .5
+		},function(earthquakes){
+			//process response
+			var featuresArr = earthquakes.features;
+			//console.log(featuresArr);
+			var currentEarthQuake = featuresArr[0];
+			postMostRecentEarthQuake(currentEarthQuake);
+	});
+}
+
+var postMostRecentEarthQuake = function(currentEarthQuake){
+	$("#most-recent-earthquake").empty();
+	document.getElementById('most-recent-earthquake').innerHTML = '<p>'+'A ' + currentEarthQuake.properties.mag + ' magnitude earthquake occured' + 
+	'<br>' + currentEarthQuake.properties.place + '<br>' + 'on ' + new Date (currentEarthQuake.properties.time).toLocaleString() +'</p>';
+
+	document.getElementById('update').innerHTML = 'Last updated at ' + new Date($.now()).toLocaleString();
+}
+
+
+getData();
+setInterval(getData, 10000);
